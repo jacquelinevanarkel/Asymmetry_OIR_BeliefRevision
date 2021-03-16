2874cdee6224822f9c34daafe4cb862feeca44ce
jacquelinevanarkel <jacquelinevanarkel@gmail.com>
2021-03-16 19:19:00 +0100

    Added two plots, but not done yet

--- plotting_2.py
+++ plotting_2.py
@@ -65,53 +65,54 @@ df_compare = pd.merge(df, df_start_asym, on="simulation_number_new")
 colors = ["#fee090", "#fdae61", "#f46d43", "#a50026", "#abd9e9", "#74add1", "#4575b4", "#313695"]
 sns.set(font_scale=1.3, palette=sns.color_palette(colors), style="whitegrid")
 
-#sns.barplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare)
-ax = sns.factorplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, colors=colors[:4])
-sns.stripplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
-
-ax.set(ylim=(-0.05,1))
-
-plt.ylabel("Remaining normalised asymmetry of networks")
-plt.xlabel("Normalised asymmetry of start networks")
-
-# # Get the handles and labels. For this example it'll be 2 tuples
-# # of length 4 each.
-# handles, labels = ax.get_legend_handles_labels()
+# #sns.barplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare)
+# ax = sns.factorplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, colors=colors[:4])
+# sns.stripplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
+# #sns.relplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, size="weight", palette=sns.color_palette(colors[4:]))
 #
-# # When creating the legend, only use the first two elements
-# # to effectively remove the last two.
-# l = plt.legend(handles[0:4], labels[0:4], loc="upper left", title="n_repair")
-
-plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
-#plt.plot([0, 0.5, 1], [0, 0.5, 1], 'o:', color='blue')
-
-plt.show()
-
-# Same but for intention
-#sns.barplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare)
-ax = sns.factorplot(x="normalised_asymmetry_intention_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare, colors=colors[:4])
-sns.stripplot(x="normalised_asymmetry_intention_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
-
-ax.set(ylim=(-0.05,1))
-
-plt.ylabel("Remaining normalised asymmetry of intention")
-plt.xlabel("Normalised asymmetry of start intention")
-plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
-#plt.plot([0, 0.5, 1], [0, 0.5, 1], 'o:', color='blue')
-
-plt.show()
-
-# Is intention communicated when the asymmetry of the network is high/low?
-ax = sns.factorplot(x="normalised_asymmetry_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare)
-sns.stripplot(x="normalised_asymmetry_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
-
-ax.set(ylim=(-0.05,1))
-
-plt.ylabel("Remaining normalised asymmetry of intention")
-plt.xlabel("Normalised asymmetry of start network")
-plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
-
-plt.show()
+# ax.set(ylim=(-0.05,1))
+#
+# plt.ylabel("Remaining normalised asymmetry of networks")
+# plt.xlabel("Normalised asymmetry of start networks")
+#
+# # # Get the handles and labels. For this example it'll be 2 tuples
+# # # of length 4 each.
+# # handles, labels = ax.get_legend_handles_labels()
+# #
+# # # When creating the legend, only use the first two elements
+# # # to effectively remove the last two.
+# # l = plt.legend(handles[0:4], labels[0:4], loc="upper left", title="n_repair")
+#
+# plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
+# #plt.plot([0, 0.5, 1], [0, 0.5, 1], 'o:', color='blue')
+#
+# plt.show()
+#
+# # Same but for intention
+# #sns.barplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare)
+# ax = sns.factorplot(x="normalised_asymmetry_intention_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare, colors=colors[:4])
+# sns.stripplot(x="normalised_asymmetry_intention_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
+#
+# ax.set(ylim=(-0.05,1))
+#
+# plt.ylabel("Remaining normalised asymmetry of intention")
+# plt.xlabel("Normalised asymmetry of start intention")
+# plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
+# #plt.plot([0, 0.5, 1], [0, 0.5, 1], 'o:', color='blue')
+#
+# plt.show()
+#
+# # Is intention communicated when the asymmetry of the network is high/low?
+# ax = sns.factorplot(x="normalised_asymmetry_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare)
+# sns.stripplot(x="normalised_asymmetry_start", y="normalised_asymmetry_intention", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
+#
+# ax.set(ylim=(-0.05,1))
+#
+# plt.ylabel("Remaining normalised asymmetry of intention")
+# plt.xlabel("Normalised asymmetry of start network")
+# plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
+#
+# plt.show()
 # ----------------------------------------------------------------------------------------------------------------------
 # ------------------------------------ perceived vs non perceived misunderstanding -------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
@@ -144,5 +145,43 @@ plt.xlabel("Normalised asymmetry of intention at start")
 # #plt.legend(labels=["perceived", "not perceived"])
 # plt.ylabel("Percentage")
 # plt.xlabel("Normalised asymmetry of intention at start")
+
+plt.show()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ------------------------------------- Repair length for different asym levels ----------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+results_10["simulation_number"] = results_10["simulation_number"] + 3600
+results_12["simulation_number"] = results_12["simulation_number"] + 7200
+
+results_new = pd.concat([results_8, results_10, results_12])
+results_new["repair_length"] = results_new["repair request"].str.len()
+results_new["start_asym"] = np.where(results_new["conversation state"] == "Start", results_new["asymmetry_intention"], np.nan)
+
+repair_length = results_new.groupby(['simulation_number', 'n_repair'])['repair_length'].mean().reset_index(name='Mean')
+df_start['simulation_number'] = df_start['simulation_number_new']
+new = pd.merge(df_start, repair_length, on="simulation_number")
+new2 = new[new['n_repair_y'] > 0]
+
+
+ax = sns.factorplot(x="normalised_asymmetry_intention_start", y="Mean", hue="n_repair_y", data=new2)
+sns.stripplot(x="normalised_asymmetry_intention_start", y="Mean", hue="n_repair_y", data=new2, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
+
+plt.show()
+# ----------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------- Frequency for different asym levels ----------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
+#sns.countplot(x="normalised_asymmetry_intention_start", hue="n_repair", data=df_compare)
+
+x,y = 'normalised_asymmetry_intention_start', 'n_repair'
+
+(df_compare
+.groupby(x)[y]
+.value_counts(normalize=True)
+.mul(100)
+.rename('percent')
+.reset_index()
+.pipe((sns.catplot,'data'), x=x,y='percent',hue=y,kind='bar'))
+
 
 plt.show()
