8cb59c3bae7a57a7dd0638ac64186601880ea1ac
jacquelinevanarkel <jacquelinevanarkel@gmail.com>
2021-03-17 12:50:05 +0100

    Finished repair length and frequency plots

--- plotting_2.py
+++ plotting_2.py
@@ -62,13 +62,16 @@ df_start_asym = df_start[["normalised_asymmetry_start", "simulation_number_new",
 #df_compare = df.join(df_start_asym.set_index('simulation_number_new'), on="simulation_number_new", rsuffix="_start")
 df_compare = pd.merge(df, df_start_asym, on="simulation_number_new")
 
+#df_size = df_compare.groupby(["normalised_asymmetry_start", "normalised_asymmetry", "n_repair"]).size().reset_index(name='Counts')
+
 colors = ["#fee090", "#fdae61", "#f46d43", "#a50026", "#abd9e9", "#74add1", "#4575b4", "#313695"]
 sns.set(font_scale=1.3, palette=sns.color_palette(colors), style="whitegrid")
 
 # #sns.barplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare)
 # ax = sns.factorplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, colors=colors[:4])
 # sns.stripplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, jitter=True, dodge=True, palette=sns.color_palette(colors[4:]))
-# #sns.relplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_compare, size="weight", palette=sns.color_palette(colors[4:]))
+# #sns.scatterplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_size, size="Counts", palette=sns.color_palette(colors[4:]))
+# #sns.relplot(x="normalised_asymmetry_start", y="normalised_asymmetry", hue="n_repair", data=df_size, size="Counts", palette=sns.color_palette(colors[4:]), kind="strip")
 #
 # ax.set(ylim=(-0.05,1))
 #
@@ -113,41 +116,41 @@ sns.set(font_scale=1.3, palette=sns.color_palette(colors), style="whitegrid")
 # plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
 #
 # plt.show()
-# ----------------------------------------------------------------------------------------------------------------------
-# ------------------------------------ perceived vs non perceived misunderstanding -------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------
-# perceived cases where repair is initiated, non perceived cases where repair is not initiated and there is some
-# asymmetry in the intention
-
-#df_start_asym = df_start[["normalised_asymmetry_intention", "simulation_number_new"]]
-#df_compare2 = df.join(df_start_asym.set_index('simulation_number_new'), on="simulation_number_new", rsuffix="_start")
-df_compare["non_perceived"] = np.where((df_compare["normalised_asymmetry_intention_start"] != 0) & (df_compare["n_repair"] == 0), True, False)
-counts = df_compare.groupby(['normalised_asymmetry_intention_start'])['non_perceived'].value_counts().reset_index(name='Counts')
-grouped_df = counts.groupby(['normalised_asymmetry_intention_start', 'non_perceived']).agg({'Counts': 'sum'})
-percents_df = grouped_df.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
-percents_df.reset_index(inplace=True)
-percents_df["total"] = 100
-percents_df_perceived = percents_df[percents_df["non_perceived"] == False]
-
-sns.barplot(x="normalised_asymmetry_intention_start", y="total", data=percents_df, ci=None, color='#bdd7e7')
-
-g = sns.barplot(x="normalised_asymmetry_intention_start", y="Counts", data=percents_df_perceived, ci=None, color='#6baed6')
-
-plt.legend(["not perceived", "perceived"])
-g.axhline(y=50, color='black', linestyle='dashed')
-plt.ylabel("Percentage")
-plt.xlabel("Normalised asymmetry of intention at start")
-
-# g = sns.barplot(x="normalised_asymmetry_intention_start", y="Counts", hue="non_perceived", data=percents_df)
+# # ----------------------------------------------------------------------------------------------------------------------
+# # ------------------------------------ perceived vs non perceived misunderstanding -------------------------------------
+# # ----------------------------------------------------------------------------------------------------------------------
+# # perceived cases where repair is initiated, non perceived cases where repair is not initiated and there is some
+# # asymmetry in the intention
 #
-# h, l = g.get_legend_handles_labels()
-# g.legend(h, ["perceived", "not perceived"])
-# #plt.legend(labels=["perceived", "not perceived"])
+# #df_start_asym = df_start[["normalised_asymmetry_intention", "simulation_number_new"]]
+# #df_compare2 = df.join(df_start_asym.set_index('simulation_number_new'), on="simulation_number_new", rsuffix="_start")
+# df_compare["non_perceived"] = np.where((df_compare["normalised_asymmetry_intention_start"] != 0) & (df_compare["n_repair"] == 0), True, False)
+# counts = df_compare.groupby(['normalised_asymmetry_intention_start'])['non_perceived'].value_counts().reset_index(name='Counts')
+# grouped_df = counts.groupby(['normalised_asymmetry_intention_start', 'non_perceived']).agg({'Counts': 'sum'})
+# percents_df = grouped_df.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
+# percents_df.reset_index(inplace=True)
+# percents_df["total"] = 100
+# percents_df_perceived = percents_df[percents_df["non_perceived"] == False]
+#
+# sns.barplot(x="normalised_asymmetry_intention_start", y="total", data=percents_df, ci=None, color='#bdd7e7')
+#
+# g = sns.barplot(x="normalised_asymmetry_intention_start", y="Counts", data=percents_df_perceived, ci=None, color='#6baed6')
+#
+# plt.legend(["not perceived", "perceived"])
+# g.axhline(y=50, color='black', linestyle='dashed')
 # plt.ylabel("Percentage")
 # plt.xlabel("Normalised asymmetry of intention at start")
-
-plt.show()
-
+#
+# # g = sns.barplot(x="normalised_asymmetry_intention_start", y="Counts", hue="non_perceived", data=percents_df)
+# #
+# # h, l = g.get_legend_handles_labels()
+# # g.legend(h, ["perceived", "not perceived"])
+# # #plt.legend(labels=["perceived", "not perceived"])
+# # plt.ylabel("Percentage")
+# # plt.xlabel("Normalised asymmetry of intention at start")
+#
+# plt.show()
+#
 # ----------------------------------------------------------------------------------------------------------------------
 # ------------------------------------- Repair length for different asym levels ----------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
@@ -164,31 +167,74 @@ new = pd.merge(df_start, repair_length, on="simulation_number")
 new2 = new[new['n_repair_y'] > 0]
 
 
-ax = sns.factorplot(x="normalised_asymmetry_intention_start", y="Mean", hue="n_repair_y", data=new2, colors=colors[1:4])
+ax = sns.factorplot(x="normalised_asymmetry_intention_start", y="Mean", hue="n_repair_y", data=new2, palette=["#fdae61", "#f46d43", "#a50026"])
 sns.stripplot(x="normalised_asymmetry_intention_start", y="Mean", hue="n_repair_y", data=new2, jitter=True, dodge=True, palette=sns.color_palette(colors[5:]))
 
 plt.ylabel("Mean normalised length of the repair initiations")
 plt.xlabel("Normalised asymmetry of intention at start")
 ax.set(ylim=(0, 1))
+plt.legend(loc="upper left", title="n_repair", bbox_to_anchor=(1.05, 1))
 
 plt.show()
-# ----------------------------------------------------------------------------------------------------------------------
-# ----------------------------------------- Frequency for different asym levels ----------------------------------------
-# ----------------------------------------------------------------------------------------------------------------------
+# # ----------------------------------------------------------------------------------------------------------------------
+# # ----------------------------------------- Frequency for different asym levels ----------------------------------------
+# # ----------------------------------------------------------------------------------------------------------------------
 #sns.countplot(x="normalised_asymmetry_intention_start", hue="n_repair", data=df_compare)
 
-x,y = 'normalised_asymmetry_intention_start', 'n_repair'
+# x,y = 'normalised_asymmetry_intention_start', 'n_repair'
+#
+# (df_compare
+# .groupby(x)[y]
+# .value_counts(normalize=True)
+# .mul(100)
+# .rename('percent')
+# .reset_index()
+# .pipe((sns.catplot,'data'), x=x,y='percent',hue=y,kind='bar'))
+#
+# plt.ylabel("Percentage")
+# plt.xlabel("Normalised asymmetry of intention at start")
+# plt.legend(loc="upper right")
+#
+# plt.show()
 
-(df_compare
-.groupby(x)[y]
-.value_counts(normalize=True)
-.mul(100)
-.rename('percent')
-.reset_index()
-.pipe((sns.catplot,'data'), x=x,y='percent',hue=y,kind='bar'))
+#df_start_asym = df_start[["normalised_asymmetry_intention", "simulation_number_new"]]
+#df_compare2 = df.join(df_start_asym.set_index('simulation_number_new'), on="simulation_number_new", rsuffix="_start")
+counts = df_compare.groupby(['normalised_asymmetry_intention_start'])['n_repair'].value_counts().reset_index(name='Counts')
+grouped_df = counts.groupby(['normalised_asymmetry_intention_start', 'n_repair']).agg({'Counts': 'sum'})
+percents_df = grouped_df.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
+percents_df.reset_index(inplace=True)
+#percents_df["total"] = 100
+
+print(percents_df)
+
+n_0 = percents_df[percents_df["n_repair"] == 0]
+n_0 = n_0.drop(columns=['n_repair'])
+n_0 = n_0.set_index("normalised_asymmetry_intention_start")
+n_1 = percents_df[percents_df["n_repair"] == 1]
+n_1 = n_1.drop(columns=['n_repair'])
+n_1 = n_1.set_index("normalised_asymmetry_intention_start")
+n_2 = percents_df[percents_df["n_repair"] == 2]
+n_2 = n_2.drop(columns=['n_repair'])
+n_2 = n_2.set_index("normalised_asymmetry_intention_start")
+n_3 = percents_df[percents_df["n_repair"] == 3]
+n_3 = n_3.drop(columns=['n_repair'])
+n_3 = n_3.set_index("normalised_asymmetry_intention_start")
+
+n_1 = n_1.add(n_0, fill_value=0)
+n_2 = n_2.add(n_1, fill_value=0)
+n_3 = n_3.add(n_2, fill_value=0)
+
+a = sns.barplot(x=n_3.index, y="Counts", data=n_3, color=colors[7])
+b = sns.barplot(x=n_2.index, y="Counts", data=n_2, color=colors[6])
+c = sns.barplot(x=n_1.index, y="Counts", data=n_1, color=colors[5])
+d = sns.barplot(x=n_0.index, y="Counts", data=n_0, color=colors[4])
 
 plt.ylabel("Percentage")
 plt.xlabel("Normalised asymmetry of intention at start")
-plt.legend(loc="upper right")
+e = plt.Rectangle((0,0),1,1,fc=colors[4], edgecolor = 'none')
+f = plt.Rectangle((0,0),1,1,fc=colors[5],  edgecolor = 'none')
+g = plt.Rectangle((0,0),1,1,fc=colors[6], edgecolor = 'none')
+h = plt.Rectangle((0,0),1,1,fc=colors[7],  edgecolor = 'none')
+plt.legend(title="n_Repair", handles=[e, f, g, h], labels=["0", "1", "2", "3"])
 
 plt.show()
